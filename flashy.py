f988eb454972cee6bc3b61422e5b5360b07df624
Rick Mays <rickmays58@gmail.com>
2022-04-28 13:01:25 -0500

    Add create_flashcards and start writing/reading json file

--- flashy.py
+++ flashy.py
@@ -3,12 +3,22 @@ from optparse import Option
 from os import system, name
 import sys
 
-def main():
-    clear()
-    greet_user()
-    option = show_menu()
-    print(option)
+flashcards = {}
 
+def main():
+    flashcards = read_flashcards_from_disk()
+
+    clear()
+    print(flashcards)
+    greet_user()
+    show_menu()
+    option = get_option()
+    process_menu_option(option)
+
+def read_flashcards_from_disk():
+    with open('flashcards.json') as json_file:
+        flashcards = json.load(json_file)
+        return flashcards
 
 def clear():
     # for windows
@@ -28,12 +38,31 @@ def show_menu():
     print("2. Create flashcards")
     print("3. Delete flashcards")
     
+def get_option():
     option = 0
     while option not in ("1", "2", "3"):
-        option = input("Please enter an option number: ")
+        option = input("Please enter an option number (1-3): ")
     return(option)
-            
 
+def process_menu_option(option):
+    if option == str(1):
+        print("Review Flashcards")
+    elif option == str(2):
+        create_flashcard()
+    elif option == str(3):
+        print("Delete Flashcard")
+    
+
+def create_flashcard():
+    while(True):
+        front = input("Please enter text for flashcard front (word, question, etc.: ")
+        back = input("Please enter text for flashcard back (definition, answer, etc.: ")
+        flashcards[front] = back
+        print(flashcards)
+        with open('flashcards.json', 'w') as outfile:
+            json.dump(flashcards, outfile)
+        front = ""
+        back = ""
 
 
 if __name__ == '__main__':
