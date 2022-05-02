c0e5b09a49d74f55176fa633ebf47a828f9cb7bd
Rick Mays <rickmays58@gmail.com>
2022-05-01 22:41:21 -0500

    Add delete_flashcards function and revise/clean up review_flashcards function

--- flashy.py
+++ flashy.py
@@ -1,4 +1,5 @@
 # from curses import flash
+from curses import flash
 import json
 from operator import truediv
 # from optparse import Option
@@ -50,15 +51,16 @@ def process_menu_option(option):
     if option == str(1):
         review_flashcards()
     elif option == str(2):
-        create_flashcard()
+        create_flashcards()
         return
     elif option == str(3):
-        print("Delete Flashcard")
+        delete_flashcards()
     elif option == str(4):
         quit()
     
 
-def create_flashcard():
+def create_flashcards():
+    clear()
     while(True):
         print("\nWhen done entering flashcards, press enter for FRONT to exit\n")
         front = input("Please enter text for flashcard FRONT (word, question, etc.: ")
@@ -74,22 +76,51 @@ def create_flashcard():
         
 def review_flashcards():
     length = len(flashcards)
+    width = 72
     key_list = list(flashcards.keys())
     value_list = list(flashcards.values())
     for i in range(0, length):
+        option = 'a'
+        while option != "":
+            clear()
+            print(key_list[i], end = "\n\n")
+            print("-"*width)
+            print("Press enter/return key to see back of flashcard\n")
+            option = input()
         clear()
-        print("Press enter/return key to see back of flashcard\n")
-        print(key_list[i])
-        input()
+        print(key_list[i], end = "\n\n")
         print(value_list[i], end = "\n\n")
+        print("-"*width)
         input_string = input("Press enter/return key for next flashcard or q to return to menu: ")
         if input_string.lower() == "q":
             return
         clear()
-        
+
+def delete_flashcards():
+    option = 'a'
+    key_to_delete = -1
+    clear()
+    for i,j in enumerate(flashcards):
+        print(f"Index: {i}, Key: {j}")
+    print("\n")
+    while not option.isdigit():
+        option = input("Enter index to be deleted or press enter/return to exit without deleting: ")
+        if option == "":
+            return
+    index_to_delete = int(option)
+    key_list = list(flashcards.keys())
+    if index_to_delete < len(key_list):
+        key_to_delete = (key_list[index_to_delete])
+    else:
+        input("Invalid key entered, press enter/return to return to menu")
+        return
+    confirm_delete = input(f"DELETE Index: {index_to_delete}, Key: {key_to_delete}? (y/n) ")
+    if confirm_delete.lower() != 'y':
+        return
+    flashcards.pop(key_to_delete, None)
+    with open('flashcards.json', 'w') as outfile:
+        json.dump(flashcards, outfile)
     
-
-
 if __name__ == '__main__':
     sys.exit(main()) 
 
